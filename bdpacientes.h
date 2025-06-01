0d861e8094f2594fc7818040119e3c156b42b9b9
Gustavo Gomes <gusgalves084@gmail.com>
2025-06-01 19:25:02 -0300

    bdpacientes.h final(?)

--- bdpacientes.h
+++ bdpacientes.h
@@ -1,10 +1,8 @@
 #ifndef BDPACIENTES_H
 #define BDPACIENTES_H
 
-#define MAX_PACIENTES 10
-
 //////////////////////////////////// ESTRUTURAS ////////////////////////////////////
-typedef struct paciente{ // define a estrutura do paciente
+typedef struct paciente { // define a estrutura do paciente
     int id;
     char cpf[15]; // xxx.xxx.xxx-xx
     char nome[100];
@@ -12,15 +10,15 @@ typedef struct paciente{ // define a estrutura do paciente
     char data_cadastro[11]; // AAAA-MM-DD
 } Paciente;
 
-typedef struct bdpaciente{ // define a estrutura do banco de dados
-    Paciente pacientes[MAX_PACIENTES];
-    int total;
+typedef struct bdpaciente { // define a estrutura do banco de dados
+    Paciente *pacientes; // ponteiro para o vetor de pacientes
+    int total; // número atual de pacientes
+    int capacidade; // capacidade inicial
 } BDPaciente;
 
 BDPaciente bd_criarBanco(); // cria e retorna o banco de dados
 
-// cria e retorna um paciente
-Paciente bd_criarPaciente(int pId, const char *pCpf, const char *pNome, int pIdade, const char *pData);
+Paciente bd_criarPaciente(int pId, const char *pCpf, const char *pNome, int pIdade, const char *pData); // cria e retorna o paciente
 
 
 
@@ -33,16 +31,15 @@ void bd_carregar_csv(BDPaciente *bd); // carrega os dados do arquivo para o banc
 void bd_busca(const BDPaciente *bd); // coleta parâmetros pra busca do paciente e chama a busca adequada
 
 // busca e retornar todas as ocorrências de um prefixo de nome
-int *bd_buscaNome(const BDPaciente *bd, const char *prefixo, int resultados[]);
+int* bd_buscaNome(const BDPaciente *bd, const char *prefixo, int *total);
 
 // busca e retornar todas as ocorrências de um prefixo de CPF
-int *bd_buscaCPF(const BDPaciente *bd, const char *prefixo, int resultados[]);
+int* bd_buscaCPF(const BDPaciente *bd, const char *prefixo, int *total);
 
 
 
 //////////////////////////////////// IMPRESSÃO DE DADOS ////////////////////////////////////
 void bd_imprimir_paciente(const Paciente *p); // imprime um paciente específico
-
 void bd_imprimir_lista(const BDPaciente *bd); // imprime a lista de pacientes
 
 
@@ -55,5 +52,6 @@ void confirma(); // aguarda o sinal do usuário para prosseguir
 void clearConsole(); // limpa o console
 
 void finalizar(BDPaciente *bd); // libera a memória alocada para o banco
+
 
 #endif
